87cae38d5bcb8b2b4b6c7749da72b7c6efff9a0d
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-26 20:59:03 +0200

    Create migrations

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function home(){
+        return view('home');
+    }
 }
--- database/migrations/2023_07_26_181145_create_trains_table.php
+++ database/migrations/2023_07_26_181145_create_trains_table.php
@@ -15,6 +15,14 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda');
+            $table->string('stazione_di_partenza');
+            $table->string('stazione_di_arrivo');
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
