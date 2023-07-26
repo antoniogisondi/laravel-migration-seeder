db1984cfffdc0cd37b25497c2727db3a41dda15c
antoniogisondi <anto.gisondi@tiscali.it>
2023-07-26 21:59:31 +0200

    Create layout

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Biglietti dei treni che hai prenotato</h1>
+            </div>
+        </div>
+    </div>
+</header>
